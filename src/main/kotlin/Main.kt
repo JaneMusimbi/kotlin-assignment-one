eb6c8870d9fe7d8ecb37547f8eca1ac7f9c470ed
JaneMusimbi <janemusimbi@gmail.com>
2022-02-15 22:01:26 -0500

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main() {
+    var name = "Jane Musimbi"
+    println(name)
+    var age = 30
+    println(age)
+    var phoneNumber = "0713464615"
+    println(phoneNumber)
+    var weight = 78
+    println(weight)
+    var kenyan = true
+    println(kenyan)
+
+
+
+}
